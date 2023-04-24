437097281adf67d2fb70a293a531467f84eba2a6
Germain SIPIERE <gsipiere@gmail.com>
2023-04-24 22:06:13 +0200

    c'est mieux

--- index.js
+++ index.js
@@ -15,15 +15,25 @@ var waitForEl = function (selector, callback, maxTimes = false) {
 
 const precedent = () => {
     console.log('précédent');
+    etape--;
+    loadContent("#content",pages[etape].html);
 }
 
 const suivant = () => {
     console.log('suivant');
+    etape++;
+    loadContent("#content",pages[etape].html);
 }
 
 const addFct = () => {
     $('#prev').click(precedent);
     $('#next').click(suivant)
+}
+
+const loadContent = (div, page) => {
+    $(div).load(page,() => {
+        $('#titre').html(pages[etape].titre)
+    });
 }
 
 
@@ -37,17 +47,27 @@ var onReadyDomElement = (element, callback) => {
       }, 100);
     }
   };
-  // example
-//   onReadyDomElement('#header', function(e) {
-//     e.html('OK');
-//   });
+  
 
 
 const init = () => {
-    $('#commandes').load('commandes.html');
-    onReadyDomElement('.btn-commandes', addFct);
+    $('#commandes').load('commandes.html',addFct);
+   // onReadyDomElement('.btn-commandes', addFct);
+    loadContent("#content",pages[etape].html)
     //$('#prev').promise().done((self) => { console.log(self.html('grut')); });
     // $.when($('#prev') != null).then(  console.log($('#prev').value) )
 }
+
+let etape = 0;
+
+const pages = [
+    {titre: "Page 1 et ça marche", html: "content/content-1.html"},
+    {titre: "Page 2", html: "content/content-2.html"},
+    {titre: "Page 3", html: "content/content-3.html"},
+
+]
+
+
+
 
 init();
